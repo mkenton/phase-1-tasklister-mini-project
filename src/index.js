1f41f381d43d819422f4848ee3bc0722f4754007
Michael <mkenton2020@icloud.com>
2021-07-24 16:16:10 -0500

    first working  version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+ let list =  document.getElementById('create-task-form')
+ list.addEventListener('submit', (e) => {
+    e.preventDefault()
+    listCreator(e.target.newTaskDescription.value)
+    list.reset()
+  })
+})
+
+function listCreator(task){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  let slct = document.getElementById("priority")
+  // li.setAttribute("id", )
+  // slct.addEventListener('change',priorityColor())
+  console.log(slct);
+  btn.addEventListener('click', taskDeleter)
+  btn.textContent = '[delete]'
+  li.textContent = `${task} `
+  li.style.color = "red"
+  // priorityColor()
+  li.appendChild(btn)
+  document.getElementById('tasks').appendChild(li)
+ 
+}
+
+
+function taskDeleter(e){
+  e.target.parentNode.remove()
+}
+
+// function priorityColor(el){
+// let li = document.getElementById("asdf");
+// console.log(li);
+// }
+// //li.style.color = textColor
+// // }
+
+// // }
+// // // 
