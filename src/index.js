38a949dc80731c4649ce201ad2e6e903e8f67440
Michael <mkenton2020@icloud.com>
2021-07-25 07:40:00 -0500

    added priority color functionality

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
- let list =  document.getElementById('create-task-form')
+let list =  document.getElementById('create-task-form')
+
  list.addEventListener('submit', (e) => {
     e.preventDefault()
     listCreator(e.target.newTaskDescription.value)
     list.reset()
   })
+
+
+let drpdown = document.getElementById("priority")
+  console.log(drpdown);
+  drpdown.addEventListener('change', (e) => {
+    e.preventDefault()
+    console.log(e)
+  })
+
 })
+
+
 
 function listCreator(task){
   let li = document.createElement('li')
   let btn = document.createElement('button')
-  let slct = document.getElementById("priority")
-  // li.setAttribute("id", )
-  // slct.addEventListener('change',priorityColor())
-  console.log(slct);
+
   btn.addEventListener('click', taskDeleter)
-  btn.textContent = '[delete]'
+  btn.textContent = 'x'
   li.textContent = `${task} `
-  li.style.color = "red"
-  // priorityColor()
+  li.style.color = `${document.getElementById("priority").value}`
   li.appendChild(btn)
   document.getElementById('tasks').appendChild(li)
  
@@ -28,6 +36,7 @@ function listCreator(task){
 function taskDeleter(e){
   e.target.parentNode.remove()
 }
+
 
 // function priorityColor(el){
 // let li = document.getElementById("asdf");
