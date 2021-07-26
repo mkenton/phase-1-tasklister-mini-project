1f61e554cb18716057878cb4a45c761d4974bb57
Michael <mkenton2020@icloud.com>
2021-07-26 02:52:46 -0500

    Added date and editable task features

--- src/index.js
+++ src/index.js
@@ -1,49 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-let list =  document.getElementById('create-task-form')
+let list =  document.getElementById('create-task-form');
 
  list.addEventListener('submit', (e) => {
-    e.preventDefault()
-    listCreator(e.target.newTaskDescription.value)
-    list.reset()
+    //console.log(e);
+    e.preventDefault();
+    listCreator(e.target.newTaskDescription.value);
+    list.reset();
   })
 
 
-let drpdown = document.getElementById("priority")
-  console.log(drpdown);
-  drpdown.addEventListener('change', (e) => {
-    e.preventDefault()
-    console.log(e)
+
   })
 
-})
+
 
 
 
 function listCreator(task){
-  let li = document.createElement('li')
-  let btn = document.createElement('button')
-
-  btn.addEventListener('click', taskDeleter)
-  btn.textContent = 'x'
-  li.textContent = `${task} `
-  li.style.color = `${document.getElementById("priority").value}`
-  li.appendChild(btn)
-  document.getElementById('tasks').appendChild(li)
+  let li = document.createElement('li');
+  
+  let btn = document.createElement('button');
+  btn.addEventListener('click', taskDeleter);
+  btn.textContent = 'x';
+  
+  let edit = document.createElement('button');
+  edit.textContent = 'edit'
+  edit.addEventListener('click', makeEditable)
+  edit.setAttribute("class","edit")
+  
+  li.textContent = `${task} | ` + ` DUE: ${document.getElementById("date").value} `;
+  li.style.color = `${document.getElementById("priority").value}`;
+  li.appendChild(edit)
+  li.appendChild(btn);
+  document.getElementById('tasks').appendChild(li);
  
+}
+
+function makeEditable(e){
+  e.target.setAttribute("class","clicked")
+  e.target.parentNode.setAttribute("contentEditable","true");
+  e.target.parentNode.setAttribute("background-color", "rgb(160, 160, 160)");
 }
 
 
 function taskDeleter(e){
-  e.target.parentNode.remove()
+  e.target.parentNode.remove();
 }
 
-
-// function priorityColor(el){
-// let li = document.getElementById("asdf");
-// console.log(li);
-// }
-// //li.style.color = textColor
-// // }
-
-// // }
-// // // 
